dfd6709d91b9582caa2225850ced1b272c868314
OsipovIvan <osipovivan23@gmail.com>
2021-03-22 23:27:52 +0300

    Задача №3 - Wall Tests

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -6,9 +6,31 @@ class WallServiceTest {
 
     @Test
     fun add() {
+        val addPost = WallService.add(Post())
+        assertNotEquals(0, addPost.id)
     }
 
     @Test
-    fun update() {
+    fun updateExisting() {
+
+        WallService.add(Post(text = "TestPost1"))
+        WallService.add(Post(text = "TestPost2"))
+        WallService.add(Post(text = "TestPost3"))
+
+        val update = Post(id = 3)
+        val result = WallService.update(update)
+        assertTrue(result)
+    }
+
+    @Test
+    fun updateNotExisting() {
+
+        WallService.add(Post(text = "TestPost1"))
+        WallService.add(Post(text = "TestPost2"))
+        WallService.add(Post(text = "TestPost3"))
+
+        val update = Post(id = 7)
+        val result = WallService.update(update)
+        assertFalse(result)
     }
 }
